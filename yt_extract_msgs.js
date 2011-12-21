18686bc98a9e83e92c84191a09404d05bb075d91
Stuart Powers <stuart.powers@gmail.com>
2011-12-21 00:30:39 -0500

    changed the setTimeout() call to reflect correct function name

--- yt_extract_msgs.js
+++ yt_extract_msgs.js
@@ -12,7 +12,7 @@ function extract_msgs(msgtype)
         tables.push(inbox.message_pane_.innerHTML);
         console.log(tables.length);
         inbox.next_page();
-        setTimeout("myfunction()",2200);
+        setTimeout("extract_msgs()",2200);
     }
     else{
         alert("Fininshed crawling:" + tables.length + " pages.");
